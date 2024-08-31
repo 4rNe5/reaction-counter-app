f17098d58f823dbd2765701951e6c34800ba2cc7
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-09-01 03:36:11 +0900

    ADD :: RankingView & PB API Logic
    And ADD README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Action! App Repo
+***
+## Project Setting
+***
+### 1. Create a new API_URL
+```bash
+```tsx
+export const API_URL = '(your_pb_url_here)';
+```
--- screens/RankingView.tsx
+++ screens/RankingView.tsx
@@ -1,10 +1,37 @@
-import { ScreenContent } from 'components/ScreenContent';
+import PocketBase from 'pocketbase';
 import { StyleSheet, View } from "react-native";
+import { API_URL } from "../api";
+import { useCallback, useEffect, useState } from "react";
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function RankingView() {
+
+  const pb = new PocketBase(API_URL);
+  const [ranking, setRanking] = useState([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchRanking = async () => {
+        try {
+          const ranking = await pb.collection('reaction_records').getFullList({
+            sort: '-reactionMs',
+          }).then((res) => {
+            console.log(res);
+            // @ts-ignore
+            setRanking(res);
+          });
+        } catch (error) {
+          console.error("Failed to fetch ranking:", error);
+        }
+      };
+
+      fetchRanking();
+    }, [])
+  );
+
   return (
     <View style={styles.container}>
-
+      {/* Render your ranking data here */}
     </View>
   );
 }
